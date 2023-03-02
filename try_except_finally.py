74b1042bbe37deb0ce1a9ed0d8b6dcf96cd614e0
April Milan <108895508+aprilmilanpdx@users.noreply.github.com>
2023-03-02 15:49:41 -0800

    final commit of refactored code

--- try_except_finally.py
+++ try_except_finally.py
@@ -44,15 +44,31 @@
 #         n_square = n ** 2
 #         return n_square
 
+
+# # this worked for me but is not the preferred way to do this
+# def power_of_two():
+#     user_input = input('Please enter a number: ')
+#     try:
+#         n = float(user_input)
+#     except ValueError:
+#         print('Your input was invalid. Using default value 0')
+#         n = 0
+#     finally:
+#         n_square = n ** 2
+#         return n_square
+    
+
+# this is the instructors preferred code
 def power_of_two():
     user_input = input('Please enter a number: ')
     try:
         n = float(user_input)
     except ValueError:
         print('Your input was invalid. Using default value 0')
-        n = 0
-    finally:
+        return 0.0
+    else:
         n_square = n ** 2
         return n_square
 
+print(power_of_two())
 print(power_of_two())
